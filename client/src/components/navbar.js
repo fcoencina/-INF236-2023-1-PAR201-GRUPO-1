817f63625a12961bcd7d5cf36c45127acabadc39
Francisco Encina <fcoencinaaba@gmail.com>
2023-05-17 01:29:55 -0400

    Cambios en apariencia

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,43 +1,53 @@
 import React from "react";
- 
+
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
- 
+
 // We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
- 
+
 // Here, we display our Navbar
 export default function Navbar() {
- return (
-   <div>
-     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-       <NavLink className="navbar-brand" to="/" style={{display:"flex",alignItems:"center",padding:10}}>
-       <img style={{"width" : 8 + '%',marginRight:10}} src="https://cdn.pixabay.com/photo/2016/01/12/23/21/paramedic-1136916_1280.png" alt=""></img>
-       <h1 style={{"text-align":+"center"}}>S.A.P.U.</h1>
-       </NavLink>
-       
-       <button
-         className="navbar-toggler"
-         type="button"
-         data-toggle="collapse"
-         data-target="#navbarSupportedContent"
-         aria-controls="navbarSupportedContent"
-         aria-expanded="false"
-         aria-label="Toggle navigation"
-       >
-         <span className="navbar-toggler-icon"></span>
-       </button>
- 
-       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul className="navbar-nav ml-auto">
-           <li className="nav-item">
-             <NavLink className="nav-link" to="/create">
-               Generar Paciente
-             </NavLink>
-           </li>
-         </ul>
-       </div>
-     </nav>
-   </div>
- );
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <NavLink className="navbar-brand" to="/" style={{ display: "flex", alignItems: "center", padding: 10 }}>
+          <img style={{ "width": 8 + '%', marginRight: 10 }} src="https://cdn.pixabay.com/photo/2016/01/12/23/21/paramedic-1136916_1280.png" alt=""></img>
+          <h1 style={{ "text-align": +"center" }}>S.A.P.U.</h1>
+        </NavLink>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">
+                Inicio
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/form">
+                Form
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/create">
+                Ingresar Paciente
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 }
--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -1,83 +1,90 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
- 
+
 const Record = (props) => (
- <tr>
-   <td>{props.record.name}</td>
-   <td>{props.record.position}</td>
-   <td>{props.record.level}</td>
-   <td>
-     <Link className="btn btn-link" to={`/edit/${props.record._id}`}>Editar</Link> |
-     <button className="btn btn-link"
-       onClick={() => {
-         props.deleteRecord(props.record._id);
-       }}
-     >Borrar</button>
-   </td>
- </tr>
+  <tr>
+    <td>{props.record.name}</td>
+    <td>{props.record.position}</td>
+    <td>{props.record.level}</td>
+    <td>
+      <Link className="btn btn-link" to={`/edit/${props.record._id}`}>Editar</Link> |
+      <button className="btn btn-link"
+        onClick={() => {
+          props.deleteRecord(props.record._id);
+        }}
+      >Borrar</button>
+    </td>
+  </tr>
 );
- 
+
 export default function RecordList() {
- const [records, setRecords] = useState([]);
- 
- // This method fetches the records from the database.
- useEffect(() => {
-   async function getRecords() {
-     const response = await fetch(`http://localhost:5000/record/`);
- 
-     if (!response.ok) {
-       const message = `An error occurred: ${response.statusText}`;
-       window.alert(message);
-       return;
-     }
- 
-     const records = await response.json();
-     setRecords(records);
-   }
- 
-   getRecords();
- 
-   return;
- }, [records.length]);
- 
- // This method will delete a record
- async function deleteRecord(id) {
-   await fetch(`http://localhost:5000/${id}`, {
-     method: "DELETE"
-   });
- 
-   const newRecords = records.filter((el) => el._id !== id);
-   setRecords(newRecords);
- }
- 
- // This method will map out the records on the table
- function recordList() {
-   return records.map((record) => {
-     return (
-       <Record
-         record={record}
-         deleteRecord={() => deleteRecord(record._id)}
-         key={record._id}
-       />
-     );
-   });
- }
- 
- // This following section will display the table with the records of individuals.
- return (
-   <div style={{margin:20}}>
-     <h3>Lista de Pacientes</h3>
-     <table className="table table-striped" style={{ marginTop: 20 }}>
-       <thead>
-         <tr>
-           <th>Nombre</th>
-           <th>Posición</th>
-           <th>Nivel</th>
-           <th>Acción</th>
-         </tr>
-       </thead>
-       <tbody>{recordList()}</tbody>
-     </table>
-   </div>
- );
+  const [records, setRecords] = useState([]);
+
+  // This method fetches the records from the database.
+  useEffect(() => {
+    async function getRecords() {
+      const response = await fetch(`http://localhost:5000/record/`);
+
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const records = await response.json();
+      setRecords(records);
+    }
+
+    getRecords();
+
+    return;
+  }, [records.length]);
+
+  // This method will delete a record
+  async function deleteRecord(id) {
+    await fetch(`http://localhost:5000/${id}`, {
+      method: "DELETE"
+    });
+
+    const newRecords = records.filter((el) => el._id !== id);
+    setRecords(newRecords);
+  }
+
+  // This method will map out the records on the table
+  function recordList() {
+    return records.map((record) => {
+      return (
+        <Record
+          record={record}
+          deleteRecord={() => deleteRecord(record._id)}
+          key={record._id}
+        />
+      );
+    });
+  }
+
+  // This following section will display the table with the records of individuals.
+  return (
+    <div style={{ margin: 20 }}>
+      <br></br>
+      <form class="d-flex">
+        <input className="form-control me-sm-2" type="search" placeholder="Buscar Paciente" />
+        <button className="btn btn-secondary my-2 my-sm-0" type="submit">Buscar</button>
+      </form>
+      <br></br>
+      <br></br>
+      <h3>Lista de Pacientes</h3>
+      <table className="table table-striped" style={{ marginTop: 20 }}>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Posición</th>
+            <th>Nivel</th>
+            <th>Acción</th>
+          </tr>
+        </thead>
+        <tbody>{recordList()}</tbody>
+      </table>
+    </div>
+  );
 }
